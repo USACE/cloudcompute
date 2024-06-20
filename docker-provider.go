4d58ea14874a110ba603a5f7a4189787e9e8477a
randal.goss <randal.s.goss@usace.army.mil>
2024-06-20 11:30:49 -0400

    docker provider changes

--- docker-provider.go
+++ docker-provider.go
@@ -9,22 +9,71 @@ import (
 type DockerProvider struct {
 	Concurrency       int
 	RegisteredPlugins []*Plugin
-	Jobs              []Job
+	Jobs              []*Job
 	JobSummaries      map[string]JobSummary
 	Logs              map[string]string
 }
 
 /*
 func NewDockerProvider(maxConcurrency int) (*DockerProvider, error) {
+	dcm := NewDockerComputeManager(DockerComputeManagerConfig{
+		Concurrency: 10,
+	})
+	dcm.StartMonitor()
+	return &DockerComputeProvider{dcm}
 
-}
-
-func (dp *DockerProvider) start() error {
-	//go function to continuously evaluate and run jobs
 }
 */
 
-//type DockerPlugin struct{}
+func (dp *DockerProvider) registeredPlugin(name string) (*Plugin, error) {
+	for _, v := range dp.RegisteredPlugins {
+		if v.Name == name {
+			return v, nil
+		}
+	}
+	return nil, errors.New("Plugin is not registered.")
+}
+
+/*
+
+func (dp *DockerProvider) start() error {
+	//go function to continuously evaluate and run jobs
+	var runtimeLimit int = 3
+	semaphore := make(chan struct{}, runtimeLimit)
+	for i := 0; i < 1000; i++ {
+		semaphore <- struct{}{}
+		go func(event int) {
+			defer func() {
+				<-semaphore
+			}()
+			//runHelloWorld()
+			fmt.Printf("Running:%d\n", event)
+		}(i)
+	}
+
+	// Wait for goroutines to finish by acquiring all slots.
+	for i := 0; i < cap(semaphore); i++ {
+		semaphore <- struct{}{}
+	}
+	return nil
+}
+*/
+
+func (dp *DockerProvider) runJob(job *Job) error {
+	//rate limited running of jobs
+	plugin, err := dp.registeredPlugin(job.JobDefinition)
+	if err != nil {
+		return err
+	}
+	runner, err := NewRunner(job, plugin)
+	if err != nil {
+		panic(err)
+	}
+	defer runner.Close()
+	runner.Start()
+
+	return nil
+}
 
 func (dp *DockerProvider) RegisterPlugin(plugin *Plugin) (PluginRegistrationOutput, error) {
 	dp.RegisteredPlugins = append(dp.RegisteredPlugins, plugin)
@@ -73,5 +122,9 @@ func (dp *DockerProvider) Status(jobQueue string, query JobsSummaryQuery) error 
 }
 
 func (dp *DockerProvider) SubmitJob(job *Job) error {
+	if job == nil {
+		return errors.New("Job cannot be nil.")
+	}
+	dp.Jobs = append(dp.Jobs, job)
 	return nil
 }
